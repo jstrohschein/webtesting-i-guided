6db35e464bd1aa285a68819f7b0d20d1ad53a4e5
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-11-11 18:38:20 +0100

    wip

--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,9 +1,8 @@
 const { add } = require('./calculator.js');
 
 describe('calculator', () => {
-  it('sanity check', () => {
-    // this is an actual (empty) test
-    expect(2 + 3).toBe(5)
-    expect(2 + 2).toBe(4)
+  it('sanity check', () => { // the test
+    expect(2 + 3).toBe(5) // an assertion
+    expect(2 + 2).toBe(4) // another assertion
   })
 })
