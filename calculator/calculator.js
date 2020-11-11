ecc1a46a7a1c7933ef6544bfe6b702a8404b8f4f
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-11-11 19:15:25 +0100

    wip

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -5,7 +5,9 @@ module.exports = {
 function add(...numbers) {
   // TDD STEP 2 - write the least amount of code
   // that will make the test pass
-  return numbers.reduce()
+  return numbers.reduce((acc, num) => {
+    acc + num
+  })
 }
 // TDD STEP 3 - now you refactor the code
 // to make it nicer (the tests have your back)
